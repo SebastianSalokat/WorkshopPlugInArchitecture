1eaf2f3c90d4c8753f072f96c67df547e626b889
SebastianSalokat <s.salokat@gmx.de>
2019-12-02 10:29:23 +0100

    Delete IPlugInLoader.cs

--- PluginManagement.Contract/IPlugInLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Company.Product.Logic.PlugInManagement.Contract
-{
-    public interface IPlugInLoader
-    {
-        void Load();
-        void RaiseMyPublicEventMessage();
-        void RaiseMySecondPublicEventMessage();
-    }
-}

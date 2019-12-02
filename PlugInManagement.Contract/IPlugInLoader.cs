20edc7d818b986024dc797a137a9d7f599c8afc5
Sebastian Salokat <s.salokat@gmx.de>
2019-12-02 10:32:39 +0100

    fix naming

---
+++ PlugInManagement.Contract/IPlugInLoader.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.Product.Logic.PlugInManagement.Contract
+{
+    public interface IPlugInLoader
+    {
+        void Load();
+        void RaiseMyPublicEventMessage();
+        void RaiseMySecondPublicEventMessage();
+    }
+}
